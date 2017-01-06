0e7db738ee9274f471c217e78da71494e8e51e2b
ecleya <ecleya@gmail.com>
2017-01-06 17:23:37 +0900

    add my dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+source ./scripts/brew
+source ./scripts/install-apps
+source ./scripts/preferences
+
 rsync --exclude ".git/" \
 	--exclude ".DS_Store" \
 	--exclude "bootstrap.sh" \
 	--exclude "README.md" \
 	--exclude "LICENSE" \
-	-avh --no-perms . ~;
+	-avh --no-perms ./dotfiles ~;
 
 source ~/.bash_profile;
---
+++ dotfiles/.bash_profile
@@ -0,0 +1,1 @@
+source .peco_history
