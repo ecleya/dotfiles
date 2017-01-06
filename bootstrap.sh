30c6d3a25ab28ccfa662b52d2fc0be7e86f17b0f
ecleya <ecleya@gmail.com>
2017-01-06 15:09:15 +0900

    let's try!

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rsync --exclude ".git/" \
+	--exclude ".DS_Store" \
+	--exclude "bootstrap.sh" \
+	--exclude "README.md" \
+	--exclude "LICENSE" \
+	-avh --no-perms . ~;
+
+source ~/.bash_profile;
