529ba0d52e11c5e8ebc319fa17176f6920fb9c18
ecleya <ecleya@gmail.com>
2017-01-09 18:23:46 +0900

    change many things...

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 source ./scripts/brew
+source ./scripts/brew-update
 source ./scripts/install-apps
 source ./scripts/preferences
 
-rsync -avh --no-perms ./dotfiles/ ~;
+ln -s ./dotfiles/.bash_profile ~/
 
 source ~/.bash_profile;
