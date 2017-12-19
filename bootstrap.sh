6d5f00e860910c4d9aaf15a77d9c0477104f6e23
ecleya <ecleya@gmail.com>
2017-12-20 02:29:15 +0900

    autodot 1차 적용.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-source ./scripts/brew-init
-source ./update.sh
+
+source ./scripts/init
+source ./scripts/brew
+source ./scripts/brew_cask
+source ./scripts/app_store
+source ./scripts/login_items
+source ./scripts/preferences
+
+cp ./dotfiles/.bash_profile ~/.bash_profile
+
+source ~/.bash_profile;
--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source ./scripts/brew
-source ./scripts/brew-update
-source ./scripts/install-apps
-source ./scripts/preferences
-
-ln -s ./dotfiles/.bash_profile ~/
-
-source ~/.bash_profile;
