70089971c376b3b84003dbc5b114f520bd084076
ecleya <ecleya@gmail.com>
2022-03-25 15:54:32 +0900

    update 2022.03.25

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,13 @@ source ./scripts/customizes
 
 cp -r ./home/ ~
 
+git clone https://github.com/ecleya/dotfiles.git ~/Projects/dotfiles
+
+mkdir -p ~/Projects
+ln -s ~/Projects/dotfiles/apps/vscode ~/Library/Application\ Support/Code/User
+
+mkdir -p ~/.config
+ln -s ~/Projects/dotfiles/apps/karabiner ~/.config/karabiner
+
 # Done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
